616382663d8ecbdcc902fdb5e262c858233b53d9
jae1 <t01046993182@gmail.com>
2026-07-13 16:41:28 +0900

    fix: 비밀번호 컬럼 매핑 수정

--- src/main/java/com/asdf/tongchoobe/domain/User.java
+++ src/main/java/com/asdf/tongchoobe/domain/User.java
@@ -34,7 +34,7 @@ public class User {
     @Column(nullable = false, unique = true, length = 100)
     private String email;
 
-    @Column(nullable = false, length = 100)
+    @Column(name = "password_hash", nullable = false, length = 255)
     private String password;
 
     @Column(nullable = false, unique = true, length = 20)
